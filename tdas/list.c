4cdf79889739c52527817f9a31efa568e7af4fe3
xthestrange <benjavivanco2001@gmail.com>
2025-06-21 19:25:13 -0400

    modificacion

--- tdas/list.c
+++ tdas/list.c
@@ -2,20 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node {
-  void *data;
-  struct Node *next;
-} Node;
-
-struct List {
-  Node *head;
-  Node *tail;
-  Node *current;
-  int size;
-};
-
-typedef List List;
-
 List *list_create() {
   List *newList = (List *)malloc(sizeof(List));
   if (newList == NULL) {
